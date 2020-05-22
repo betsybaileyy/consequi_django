780d83cdc8307b33922916b3474b9dd4d1ed5376
betsybaileyy <betsy.bailey80@gmail.com>
2020-05-22 02:30:03 -0400

    troubleshooting buddy null error on achievement

--- goal/views.py
+++ goal/views.py
@@ -6,4 +6,26 @@ from goal.models import Achievement, Goal
 def index(request):
     all_achievements = Achievement.objects.filter(name="Hello World")
     all_goals = Goal.objects.all()
+    # return render(request, 'templates/homeScreen.html')
     return HttpResponse(f"# of Achievements: <br># of Goals: {all_achievements}<br> # of goals: {len(all_goals)} ")
+
+
+
+
+# def vote(request, goal_id):
+#     goal = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
